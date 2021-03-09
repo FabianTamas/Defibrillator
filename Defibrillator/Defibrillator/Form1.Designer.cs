12d7cf5877c658be2faebe246c22401c9e4cdee7
FabianTamas <70685254+FabianTamas@users.noreply.github.com>
2021-03-09 19:58:19 +0100

    Add files via upload

---
+++ Defibrillator/Defibrillator/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Defibrillator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBeolvas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBeolvas
+            // 
+            this.btnBeolvas.Location = new System.Drawing.Point(56, 26);
+            this.btnBeolvas.Name = "btnBeolvas";
+            this.btnBeolvas.Size = new System.Drawing.Size(118, 32);
+            this.btnBeolvas.TabIndex = 0;
+            this.btnBeolvas.Text = "Beolvasás";
+            this.btnBeolvas.UseVisualStyleBackColor = true;
+            this.btnBeolvas.Click += new System.EventHandler(this.btnBeolvas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.btnBeolvas);
+            this.Name = "Form1";
+            this.Text = "Helymeghatározás";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBeolvas;
+    }
+}
+
---
+++ Defibrillator/Defibrillator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Defibrillator
+{
+    public partial class Form1 : Form
+    {
+        static List<Keszulek> keszulekek = new List<Keszulek>();
+
+        public void Beolvas()
+        {
+            StreamReader be = new StreamReader("data.csv");
+            be.ReadLine();
+            while (!be.EndOfStream)
+            {
+                string[] a = be.ReadLine().Split(';');
+                keszulekek.Add(new Keszulek(int.Parse(a[0]), a[1], double.Parse(a[2]), double.Parse(a[3]), a[4], a[5]));
+            }
+            be.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Beolvas();
+        }
+
+        private void btnBeolvas_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
